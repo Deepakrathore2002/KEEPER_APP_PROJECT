2b035c7e6a4181ee1f66e138455aa1be05b2615b
Deepakrathore2002 <93067220+Deepakrathore2002@users.noreply.github.com>
2023-01-20 08:50:50 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from '../notes.js';
+
+
+function App() {
+  return (
+    <div>
+     <Header  />
+    { notes.map( term => <Note
+       key={term.key}
+       title={term.title}
+       content={term.content}
+       />)}
+       
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
